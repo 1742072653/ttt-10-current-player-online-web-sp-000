82cb15a3460afeb68f5af0df78713288175c57a9
Wenhao Guo <gwh1742072653@gmail.com>
2020-10-24 17:47:35 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  truns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+
+def current_player(board)
+  turns = trun_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+    
+      
